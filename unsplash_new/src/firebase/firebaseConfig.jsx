74ebffcc80880184e29b00a05f9aa51098213a7c
Jonibek Orifjonov  Web Developer <147488141+Jonibek011@users.noreply.github.com>
2025-06-14 16:58:52 +0500

    .env file added

--- unsplash_new/src/firebase/firebaseConfig.jsx
+++ unsplash_new/src/firebase/firebaseConfig.jsx
@@ -3,12 +3,12 @@ import { getStorage } from "firebase/storage";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyBCsDKDROD2YA2mGmdC6mdEY0XO7L5az8k",
-  authDomain: "splash2-d2ab6.firebaseapp.com",
-  projectId: "splash2-d2ab6",
-  storageBucket: "splash2-d2ab6.appspot.com",
-  messagingSenderId: "49975168926",
-  appId: "1:49975168926:web:cfe4e89b65366c07ea908f",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
